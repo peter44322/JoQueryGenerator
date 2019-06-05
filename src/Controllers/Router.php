66822e430873d792ba8f3cace782d1ccca490e7f
peter44322 <p.pator@outlook.com>
2019-06-05 18:01:27 +0200

    add abstract class

--- src/Controllers/Router.php
+++ src/Controllers/Router.php
@@ -12,38 +12,21 @@ use Peterzaccha\JoQueryGenerator\Services\JoQueryGenerator;
 use phpDocumentor\Reflection\DocBlock\Tags\Throws;
 use ReflectionClass;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Yajra\DataTables\DataTables;
 
 class Router extends Controller
 {
-    /**
-     * @param Request $request
-     * @param $slug
-     * @return mixed
-     */
+
     public function call(Request $request, $slug){
 
-        foreach (glob(app_path().'/Datatables/*.php') as $file)
+        $class = 'App\Datatables\\'.$slug;
+
+        if (class_exists($class))
         {
-
-            require_once $file;
-
-            // get the file name of the current file without the extension
-            // which is essentially the class name
-            $class = 'App\Datatables\\'.basename($file, '.php');
-
-
-
-            if (class_exists($class))
-            {
-                $obj = new $class;
-
-                if($obj->slug() === $slug){
-                    $qg = new JoQueryGenerator($request,$obj);
-                    return  Datatables::of($qg->render())->make();
-                }
-            }
+            $obj = new $class;
+            $qg = new JoQueryGenerator($request,$obj);
+            return  Datatables::of($qg->render())->make();
         }
-
         throw new NotFoundHttpException();
     }
 }
