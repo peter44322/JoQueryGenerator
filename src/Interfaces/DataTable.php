ebc7914d19ed5e6d701712bf016a662786a72c28
peter44322 <p.pator@outlook.com>
2019-08-21 14:43:26 +0200

    fix compatibilaty issue

--- src/Interfaces/DataTable.php
+++ src/Interfaces/DataTable.php
@@ -24,7 +24,7 @@ class DataTable
     public static function titles(){
         return [];
     }
-    public static function query(){
+    public static function query($request){
         return null;
     }
 
